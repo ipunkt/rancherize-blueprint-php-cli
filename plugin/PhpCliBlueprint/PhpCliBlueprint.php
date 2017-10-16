856400d9ed1c3bc377063f6f1e8dbc21e920b212
Bastian Rang <justb81@users.noreply.github.com>
2017-10-16 15:32:35 +0200

    Update PhpCliBlueprint.php

--- plugin/PhpCliBlueprint/PhpCliBlueprint.php
+++ plugin/PhpCliBlueprint/PhpCliBlueprint.php
@@ -200,11 +200,11 @@ class PhpCliBlueprint implements Blueprint, TakesDockerAccount {
         $dockerfile->copy('.'.$copySuffix, $this->targetDirectory .$targetSuffix);
 
         if ($config->get('add-composer', false)) {
-            $dockerfile->run('php -r "copy(\'https://getcomposer.org/installer\', \'composer-setup.php\');" \
-	&& php -r "if (hash_file(\'SHA384\', \'composer-setup.php\') === \'669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410\') { echo \'Installer verified\'; } else { echo \'Installer corrupt\'; unlink(\'composer-setup.php\'); } echo PHP_EOL;" \
-	&& php composer-setup.php \
-	php -r "unlink(\'composer-setup.php\');" \
-	&& cd /var/www/app && ./composer.phar install && rm composer.phar');
+		/**
+		 * use composer programmatical
+		 * see https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+		 */ 
+            $dockerfile->run('curl -sSL "https://gist.githubusercontent.com/justb81/1006b89e41e41e1c848fe91969af7a0b/raw/c12faf968e659356ec1cb53f313e7f8383836be3/getcomposer.sh" | sh && COMPOSER_ALLOW_SUPERUSER=1 ./composer.phar install --no-dev && rm composer.phar');
         }
 
         $additionalFiles = $config->get('add-files');
